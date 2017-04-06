138ddce4560c0d38b93815899267e0af480abcf1
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2017-04-06 10:44:38 +0100

    partly working pdb viewer

--- ngl/simple.js
+++ ngl/simple.js
@@ -9,9 +9,6 @@ function buildNglViewer(divid, data) {
     //console.log("Container is " + viewer + " -> " + viewer_id);
     _fixSizes(outerDiv, viewer);
 
-
-    var if1El =  $("#format1");
-
     var stage = viewer[0].nglviewer;
     if (!stage) {
         console.log("Creating new stage in " + viewer_id);
@@ -28,55 +25,121 @@ function buildNglViewer(divid, data) {
         var d = data[i];
         if (d) {
             inputsCount++;
-
-            _loadSdf(divid, stage, d, i, controlsEl);
-
-
-//                comp.structure.eachEntity(function(entity) {
-//                    console.log("Entity: " + entity.type + " " + entity.description);
-//                });
-//
-//                comp.structure.eachChain(function(c) {
-//                    console.log("Chain: " + c.chainid + " " + c.chainname + " " + c.entity.description);
-//                });
-//
-//                comp.structure.eachPolymer(function(p) {
-//                    console.log("Polymer: " + p.residueIndexStart + " - " + p.residueIndexEnd);
-//                });
-
-//            });
-
+            if (d.extension === 'sdf') {
+                _loadSdf(divid, stage, d, i, controlsEl);
+            } else if (d.extension === 'pdb') {
+                _loadPdb(divid, stage, d, i, controlsEl);
+            }
             console.log("Loaded data" + (i+1) + " as " + d.extension);
-
         } else {
             console.log("No data" + (i+1));
             var el = controlsEl.find("div.input" + (i+1));
             el.find("*").remove();
             el.append("<span>No data loaded</span>");
         }
-
     }
-
 }
 
 var representationParams = new Map();
 representationParams.set("ball+stick", { multipleBond: true });
 
+function _loadPdb(outerId, stage, data, index, controlsEl) {
+
+    var i = index + 1;
+     _setupPdbControls(outerId, index, controlsEl);
+
+     var displayFilterEl = controlsEl.find(".components" + i);
+     var representationToUse = controlsEl.find('input[name=display_radio_' + i + ']:checked').val();
+     console.log("Using representation " + representationToUse);
+
+    var stringBlob = new Blob( [ data.molecules ], { type: data.mediaType} );
+    stage.loadFile( stringBlob, { ext: data.extension, name: "input" + i} ).then( function( comp ) {
+
+        comp.addRepresentation(representationToUse);
+        comp.autoView();
+
+        displayFilterEl.find("*").remove();
+        displayFilterEl.append(
+            '<div class="field"><div class="ui checkbox"><input type="checkbox" checked name="waters' +
+            '" onchange="macromolChanged(\'' + outerId + '\', this, ' + i + '); return false;">' +
+            '<label>Waters</label></div></div>');
+        displayFilterEl.append(
+            '<div class="field"><div class="ui checkbox"><input type="checkbox" checked name="ions' +
+            '" onchange="macromolChanged(\'' + outerId + '\', this, ' + i + '); return false;">' +
+            '<label>Ions</label></div></div>');
+        displayFilterEl.append(
+            '<div class="field"><div class="ui checkbox"><input type="checkbox" checked name="ligands' +
+            '" onchange="macromolChanged(\'' + outerId + '\', this, ' + i + '); return false;">' +
+            '<label>Ligands</label></div></div>');
+
+        var chainNames = new Set();
+        comp.structure.eachChain(function(c) {
+            console.log("Chain: " + c.chainid + " " + c.chainname + " " + c.entity.description);
+            chainNames.add(c.chainname);
+        });
+//        chainNames.forEach(function(c) {
+//            console.log("Found chain " + c);
+//            displayFilterEl.append(
+//                '<div class="field"><div class="ui checkbox"><input type="checkbox" checked name="chain_' + c +
+//                '" onchange="macromolDisplay(\'' + outerId + '\', this, ' + i + '); return false;">' +
+//                '<label>Chain ' + c + '</label></div></div>');
+//        });
+
+        comp.structure.eachPolymer(function(p) {
+            console.log("Polymer: " + p.residueIndexStart + " - " + p.residueIndexEnd);
+        });
+    });
+}
+
+function _setupPdbControls(outerId, index, el) {
+    console.log("Handling PDB controls");
+
+    var i = index + 1;
+    var onchange = "macromolChanged('" + outerId + "', this, " + i + "); return false;";
+
+    var controlsEl = el.find("div.pdb.controls" + i);
+    if (controlsEl.length == 0) {
+
+        var div = el.find("div.input" + i);
+
+        div.find("*").remove();
+
+        div.append('<div class="ui vertical accordion menu controls' + i + ' pdb">\n' +
+            '<div class="item representation' + i + '">\n' +
+            '<a class="active title"><i class="dropdown icon"></i>Display type</a>\n' +
+            '<div class="active content"><div class="ui form"><div class="grouped fields">\n' +
+            _createRepresentationRadio(outerId, "ball+stick", "Ball & Stick", i, true, onchange) +
+            _createRepresentationRadio(outerId, "cartoon", "Cartoon", i, false, onchange) +
+            _createRepresentationRadio(outerId, "hyperball", "Hyperball", i, false, onchange) +
+            _createRepresentationRadio(outerId, "licorice", "Licorice", i, false, onchange) +
+            _createRepresentationRadio(outerId, "spacefill", "Spacefill", i, false, onchange) +
+            '</div></div></div></div>\n' +
+            '<div class="item"><a class="title"><i class="dropdown icon"></i>Components</a><div class="content"><div class="ui form"><div class="grouped fields components' + i + '">\n' +
+            '</div></div></div></div></div>');
+
+        var acc = div.find('div.ui.accordion');
+        acc.accordion();
+    } else {
+        console.log("PDB controls already present");
+    }
+}
 
 function _loadSdf(outerId, stage, data, index, controlsEl) {
 
     var i = index + 1;
 
     _setupSdfControls(outerId, index, controlsEl);
-    var displayFilterEl = controlsEl.find(".molecules" + i);
 
+    var displayFilterEl = controlsEl.find(".molecules" + i);
     var representationToUse = controlsEl.find('input[name=display_radio_' + i + ']:checked').val();
     console.log("Using representation " + representationToUse);
 
     var stringBlob = new Blob( [ data.molecules ], { type: data.mediaType} );
     stage.loadFile( stringBlob, { ext: data.extension, name: "input" + i} ).then( function( comp ) {
+
         comp.addRepresentation(representationToUse, representationParams.get(representationToUse));
         comp.autoView();
+
         var mol = 1
         displayFilterEl.find("*").remove();
         comp.structure.eachModel(function(model) {
@@ -95,6 +158,7 @@ function _setupSdfControls(outerId, index, el) {
     console.log("Handling SDF controls");
 
     var i = index + 1;
+    var onchange = "representationChanged('" + outerId + "', this, " + i + "); return false;";
 
     var controlsEl = el.find("div.sdf.controls" + i);
     if (controlsEl.length == 0) {
@@ -107,13 +171,14 @@ function _setupSdfControls(outerId, index, el) {
             '<div class="item representation' + i + '">\n' +
             '<a class="active title"><i class="dropdown icon"></i>Display type</a>\n' +
             '<div class="active content"><div class="ui form"><div class="grouped fields">\n' +
-            _createRepresentationRadio(outerId, "ball+stick", "Ball & Stick", i, true) +
-            _createRepresentationRadio(outerId, "cartoon", "Cartoon", i, false) +
-            _createRepresentationRadio(outerId, "hyperball", "Hyperball", i, false) +
-            _createRepresentationRadio(outerId, "licorice", "Licorice", i, false) +
-            _createRepresentationRadio(outerId, "spacefill", "Spacefill", i, false) +
+            _createRepresentationRadio(outerId, "ball+stick", "Ball & Stick", i, true, onchange) +
+            _createRepresentationRadio(outerId, "cartoon", "Cartoon", i, false, onchange) +
+            _createRepresentationRadio(outerId, "hyperball", "Hyperball", i, false, onchange) +
+            _createRepresentationRadio(outerId, "licorice", "Licorice", i, false, onchange) +
+            _createRepresentationRadio(outerId, "spacefill", "Spacefill", i, false, onchange) +
             '</div></div></div></div>\n' +
-            '<div class="item"><a class="title"><i class="dropdown icon"></i>Molecules</a><div class="content"><div class="ui form"><div class="grouped fields molecules' + i + '">\n' +
+            '<div class="item"><a class="title"><i class="dropdown icon"></i>Molecules</a><div class="content">' +
+            '<div class="ui form"><div class="grouped fields molecules' + i + '">\n' +
             '</div></div></div></div></div>');
 
         var acc = div.find('div.ui.accordion');
@@ -123,19 +188,10 @@ function _setupSdfControls(outerId, index, el) {
     }
 }
 
-function _createRepresentationRadio(outerId, value, label, index, checked) {
+function _createRepresentationRadio(outerId, value, label, index, checked, onchange) {
     return '<div class="field"><div class="ui radio checkbox">' +
-    '<input type="radio" name="display_radio_' + index + '" value="' + value + '" onchange="representationChanged(\'' + outerId +
-    '\', this, ' + index +
-    '); return false;"' + (checked ? ' checked' : '') + '><label>' + label +'</label></div></div>\n';
-
-}
-
-function _createDisplayOption(value, text) {
-    var opt = document.createElement("option");
-    opt.value = value;
-    opt.text = text;
-    return opt;
+    '<input type="radio" name="display_radio_' + index + '" value="' + value + '" onchange="' + onchange + '"'
+     + (checked ? ' checked' : '') + '><label>' + label +'</label></div></div>\n';
 }
 
 function fitNglViewer(divid) {
@@ -146,13 +202,14 @@ function fitNglViewer(divid) {
     var stage = viewer[0].nglviewer;
     if (stage) {
         stage.handleResize();
-        stage.handleResize();
     } else {
         console.log("No stage")
     }
 }
 
+/** onchange handler for SDF files */
 function representationChanged(divid, what, input) {
+
     var representation = what.value;
     console.log("Representation changed: " + representation + " " + input);
     var viewer = $("#" + divid + " .main");
@@ -174,6 +231,64 @@ function representationChanged(divid, what, input) {
     }
 }
 
+/** onchange handler for PDB files */
+function macromolChanged(divid, what, input) {
+
+    var i = input + 1;
+
+    var representationToUse = $('#' + divid + ' .representation' + input +' input[name=display_radio_' + input + ']:checked').val();
+    console.log("Macromol changed: " + representationToUse + " " + input);
+    var viewer = $("#" + divid + " .main");
+    if (viewer.length == 0) {
+        console.log("Can't find viewer");
+    } else {
+        var stage = viewer[0].nglviewer;
+        if (!stage) {
+            console.log("Viewer not yet configured");
+            return;
+        } else {
+            stage.eachComponent(function(comp) {
+                if (comp.name === "input" + input) {
+                    comp.removeAllRepresentations();
+                    var displayWaters = $("#" + divid + ' .components' + input + ' input[name=waters' + ']').prop('checked');
+                    var displayIons = $("#" + divid + ' .components' + input + ' input[name=ions' + ']').prop('checked');
+                    var displayLigands = $("#" + divid + ' .components' + input + ' input[name=ligands' + ']').prop('checked');
+                    //console.log("Display waters=" + displayWaters + " ions=" + displayIons + " ligands=" + displayLigands);
+                    var selector = _createSelector(undefined, displayWaters, displayIons, displayLigands)
+                    console.log("Selector: " + selector);
+
+                    comp.setSelection(selector).addRepresentation(representationToUse, representationParams.get(representationToUse));
+                }
+            });
+        }
+    }
+}
+
+function _createSelector(index, displayWaters, displayIons, displayLigands) {
+    var selector;
+    if (index) {
+        selector = "/" + index;
+    }
+
+    if (!displayWaters && !displayIons && !displayLigands) { // none
+        return (selector ? selector + " " : "") + "not hetero";
+    } else if (!displayWaters && !displayIons && displayLigands) { // ligands only
+        return (selector ? selector + " " : "") + "not (water or ion)";
+    } else if (!displayWaters && displayIons && !displayLigands) { // ions only
+        return (selector ? selector + " " : "") + "not (hetero and not ion)";
+    } else if (displayWaters && !displayIons && !displayLigands) { // waters only
+        return (selector ? selector + " " : "") + "not (hetero and not water)";
+    } else if (!displayWaters && displayIons && displayLigands) { // ligands and ions
+        return (selector ? selector + " " : "") + "not water";
+    } else if (displayWaters && !displayIons && displayLigands) { // ligands and waters
+        return (selector ? selector + " " : "") + "not (hetero and ion)";
+    } else if (displayWaters && displayIons && !displayLigands) { // ions and waters
+        return (selector ? selector + " " : "") + "not (hetero and (ion or water)";
+    } else { // everything
+        return selector ? selector : "*";
+    }
+}
+
 function toggleDisplay(divid, what, input, molNumber) {
 
     var name = what.name;
@@ -191,7 +306,6 @@ function toggleDisplay(divid, what, input, molNumber) {
             var i = 1;
             stage.eachComponent(function(comp) {
                 if (comp.name === "input" + input) {
-                    comp.setSelection("/0").setVisibility(true);
                     //comp.setVisibility(true);
                     var sel = "/" + (molNumber - 1);
                     viz = comp.setSelection(sel);
@@ -209,15 +323,10 @@ function _fixSizes(outerDiv, viewer) {
     var headers = outerDiv.find(".headers");
     var status = outerDiv.find(".extra.content.line");
     var controls = outerDiv.find(".controls");
-    //var outerW = outerDiv.css("width").replace("px", "");
     var outerW = outerDiv.width();
-    //var controlsW = controls.css("width").replace("px", "");
     var controlsW = controls.width();
-    //var houter = outerDiv.css("height");
     var houter = outerDiv.height();
-    //var headersH = headers.css("height").replace("px", "");
     var headersH = headers.height();
-    //var statusH = status.css("height").replace("px", "");
     var statusH = status.height();
     var h = houter - headersH - statusH - 6;
     var w = outerW - controlsW - 2;
